4d604f2925d74b01e9a03eaba7cb1ff66e55440a
VirgilClyne <VersusClyne@gmail.com>
2021-10-21 11:45:00 +0800

    Update Apple_News.js

--- js/Apple_News.js
+++ js/Apple_News.js
@@ -1,26 +1,25 @@
 /*
-README：https://github.com/VirgilClyne/iRingo
- */
-
-const path1 = "/v1/configs";
-const path2 = "/analyticseventsv2/async";
+README:https://github.com/VirgilClyne/iRingo
+*/
 
 const url = $request.url;
 var body = $request.body;
 
+const path1 = "/v1/configs";
+const path2 = "/analyticseventsv2/async";
+
 if (url.indexOf(path1) != -1) {
-    var obj = JSON.parse(body);
-    if (obj.countryCode) countryCode = "US";
+    let obj = JSON.parse(body);  
+        obj["deviceInfo"]["preferredLanguages"] = ["zh-CN","zh-HK","zh-US","en-US"];
+        obj["deviceInfo"]["countryCode"] = "US";
     body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path2) != -1) {
-    let SIM = /"mobileData": \{.*\}/;
-    body = body.match(SIM);
-    var obj = JSON.parse(body);
-    if (obj.countryCode) obj.countryCode = "310";
-    if (obj.carrier) obj.carrier = "Google Fi";
-    if (obj.networkCode) obj.networkCode = "260";
+    let obj = JSON.parse(body);
+        obj["data"]["mobileData"]["countryCode"] = "310";
+        obj["data"]["mobileData"]["carrier"] = "Google Fi";
+        obj["data"]["mobileData"]["networkCode"] = "260";
     body = JSON.stringify(obj);
 }
 
