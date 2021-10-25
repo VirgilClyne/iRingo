4ee609f1b2e30d83c0fdd5df0e6b7699c9fa472b
VirgilClyne <VersusClyne@gmail.com>
2021-10-25 16:43:43 +0800

    Update Apple_News.js

--- js/Apple_News.js
+++ js/Apple_News.js
@@ -9,18 +9,18 @@ const path1 = "/v1/configs";
 const path2 = "/analyticseventsv2/async";
 
 if (url.indexOf(path1) != -1) {
-    let obj = JSON.parse(body);  
-        obj["deviceInfo"]["preferredLanguages"] = ["zh-CN","zh-HK","zh-US","en-US"];
-        obj["deviceInfo"]["countryCode"] = "US";
-    body = JSON.stringify(obj);
+    var configs = JSON.parse(body);  
+        configs["deviceInfo"]["preferredLanguages"] = ["zh-CN","zh-HK","zh-US","en-US"];
+        configs["deviceInfo"]["countryCode"] = "US";
+    body = JSON.stringify(configs);
 };
 
 if (url.indexOf(path2) != -1) {
-    let obj = JSON.parse(body);
-        obj["data"]["session"]["mobileData"]["countryCode"] = "310";
-        obj["data"]["session"]["mobileData"]["carrier"] = "Google Fi";
-        obj["data"]["session"]["mobileData"]["networkCode"] = "260";
-    body = JSON.stringify(obj);
+    var async = JSON.parse(body);
+        async["data"]["session"]["mobileData"]["countryCode"] = "310";
+        async["data"]["session"]["mobileData"]["carrier"] = "Google Fi";
+        async["data"]["session"]["mobileData"]["networkCode"] = "260";
+    body = JSON.stringify(async);
 };
 
 $done({body});
