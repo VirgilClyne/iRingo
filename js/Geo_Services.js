b8fa0ccd6369c5ae205c2ad741fa2c06453098aa
VirgilClyne <VersusClyne@gmail.com>
2021-10-27 16:17:18 +0800

    Revert "Revert "Update Geo_Services.js, supported argument""
    This reverts commit c7fe288153daf5679b44c7045e4058436aa2bc78.

--- js/Geo_Services.js
+++ js/Geo_Services.js
@@ -1,6 +1,13 @@
 /*
 README:https://github.com/VirgilClyne/iRingo
 */
+let GeoCountryCode = "US";
+
+if (typeof $argument != "undefined") {
+    let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+    console.log(JSON.stringify(arg));
+    GeoCountryCode = arg.GeoCountryCode;
+};
 
 const url = $request.url;
 var body = $response.body;
@@ -9,7 +16,7 @@ const path1 = "/pep/gcc";
 
 if (url.indexOf(path1) != -1) {
     let obj = body; 
-        obj = "US";
+        obj = GeoCountryCode;
     body = obj;
 };
 
