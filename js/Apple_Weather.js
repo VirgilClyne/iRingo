e5cd9fe867b360ea3df2a42d5279975abde871a3
VirgilClyne <VersusClyne@gmail.com>
2021-10-29 14:03:28 +0800

    Update Apple_Weather.js

--- js/Apple_Weather.js
+++ js/Apple_Weather.js
@@ -83,6 +83,12 @@ $done({body});
 function search(lat, lng) {   
     getJSON(
       "//api.waqi.info/mapq/nearest/?&geo=1/" + lat + "/" + lng,
+      function(err, data) {
+        if (err !== null) {
+          alert('Something went wrong: ' + err);
+        } else {
+          alert('Your query count: ' + data.query.count);
+        }
       );
     console.log("Geo:" + lat + "/" + lng );
 }
@@ -92,6 +98,29 @@ function search(lat, lng) {
 function showStation(idx) {   
     getJSON(
       "//api.waqi.info/api/feed/@" + idx + "/aqi.json",
+      function(err, data) {
+        if (err !== null) {
+          alert('Something went wrong: ' + err);
+        } else {
+          alert('Your query count: ' + data.query.count);
+        }
       );
     console.log("station.uid:" + idx );
 }
+
+
+//https://stackoverflow.com/posts/35970894/revisions
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
