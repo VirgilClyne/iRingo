691fc18878a6838f77c3561b7dbf4fffdfdd1d0d
WordlessEcho <wordless@echo.moe>
2022-04-24 00:01:15 +0800

    change text of log

--- js/Weather.beta.js
+++ js/Weather.beta.js
@@ -1162,7 +1162,7 @@ async function outputAQI(api, now, obs, weather, Settings) {
 
 	if (apiVersion === "v1") {
 		$.log(`🚧 ${$.name}, 检测到API版本为${apiVersion}，适配尚处于测试阶段，将输出修改后的内容。`, "");
-		$.log(`🚧 ${$.name}, (edited) next_hour = ${JSON.stringify(nextHour)}`, "");
+		$.log(`🚧 ${$.name}, (edited) nextHour = ${JSON.stringify(nextHour)}`, "");
 	}
 
 	switch (apiVersion) {
