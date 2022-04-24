331262978061e99c3c8b3d5ba3c0e098f89c3e12
Virgil Clyne <Virgil@nanocat.me>
2022-04-24 12:22:38 +0800

    Move out_of_china function
    Update Weather.beta.js

--- js/Weather.beta.js
+++ js/Weather.beta.js
@@ -126,20 +126,6 @@ async function getStatus(data) {
 };
 
 /**
- * https://github.com/wandergis/coordtransform/blob/master/index.js#L134
- * 判断是否在国内
- * @param lng
- * @param lat
- * @returns {boolean}
- */
-function out_of_china(lng, lat) {
-  var lat = +lat;
-  var lng = +lng;
-  // 纬度 3.86~53.55, 经度 73.66~135.05 
-  return !(lng > 73.66 && lng < 135.05 && lat > 3.86 && lat < 53.55);
-};
-
-/**
  * WAQI
  * @author VirgilClyne
  * @param {String} type - type
@@ -329,7 +315,7 @@ function getGridWeatherMinutely(lat, lng) {
 /**
  * Output Air Quality Data
  * @author VirgilClyne
- * @param {String} apiVersion - Apple API Version
+ * @param {String} apiVersion - Apple Weather API Version
  * @param {Object} now - now weather data from Third-Party
  * @param {Object} obs - observation station data from Third-Party
  * @param {Object} weather - weather data from Apple
@@ -957,11 +943,11 @@ function convertTime(time, action, apiVersion) {
 		default:
 			$.log(`⚠️ ${$.name}, Time Converter, Error`, `time: ${time}`, '');
 	}
-	if (api == "v1") {
+	if (apiVersion == "v1") {
 		let timeString = time.getTime() / 1000;
 		return timeString;
 	}
-	if (api == "v2") {
+	if (apiVersion == "v2") {
 		let timeString = time.toISOString().split('.')[0] + 'Z';
 		return timeString;
 	}
@@ -981,10 +967,7 @@ function classifyAirQualityLevel(aqiIndex) {
 	else if (aqiIndex >= 151 && aqiIndex <= 200) return 4;
 	else if (aqiIndex >= 201 && aqiIndex <= 300) return 5;
 	else if (aqiIndex >= 301 && aqiIndex <= 500) return 6;
-	else {
-		$.log(`⚠️ ${$.name}, classifyAirQualityLevel, Error`, `aqiIndex: ${aqiIndex}`, '');
-		return 6;
-	}
+	else return 6;
 };
 
 /***************** Env *****************/
