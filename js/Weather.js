aac089b9acf2ad12e4eea4f01607ee8bd6b77e23
WordlessEcho <wordless@echo.moe>
2022-05-14 15:02:12 +0800

    compatible with old settings

--- js/Weather.js
+++ js/Weather.js
@@ -202,7 +202,7 @@ const WEATHER_STATUS = {
 	/***************** Prase *****************/
 	Settings.Switch = JSON.parse(Settings.Switch) // BoxJs字符串转Boolean
 	Settings.NextHour.Switch = JSON.parse(Settings.NextHour.Switch) // BoxJs字符串转Boolean
-	Settings.NextHour.HTTPHeaders = JSON.parse(Settings.NextHour.HTTPHeaders) // BoxJs字符串转Object
+	Settings.NextHour.HTTPHeaders = Settings.NextHour?.HTTPHeaders ? JSON.parse(Settings.NextHour.HTTPHeaders) : Settings.NextHour.HTTPHeaders // BoxJs字符串转Object
 	Settings.AQI.Switch = JSON.parse(Settings.AQI.Switch) // BoxJs字符串转Boolean
 	Settings.Map.AQI = JSON.parse(Settings.Map.AQI) // BoxJs字符串转Boolean
 	$.log(`🎉 ${$.name}, Set Environment Variables`, `Settings: ${typeof Settings}`, `Settings内容: ${JSON.stringify(Settings)}`, "");
