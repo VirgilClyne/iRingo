3e530bb02458e2b0367c1ca8210ac73969e0e5aa
WordlessEcho <wordless@echo.moe>
2022-05-14 15:04:06 +0800

    format codes

--- js/Weather.beta.js
+++ js/Weather.beta.js
@@ -216,7 +216,8 @@ const WEATHER_STATUS = {
 	/***************** Prase *****************/
 	Settings.Switch = JSON.parse(Settings.Switch) // BoxJs字符串转Boolean
 	Settings.NextHour.Switch = JSON.parse(Settings.NextHour.Switch) // BoxJs字符串转Boolean
-	Settings.NextHour.HTTPHeaders = Settings.NextHour?.HTTPHeaders ? JSON.parse(Settings.NextHour.HTTPHeaders) : Settings.NextHour.HTTPHeaders // BoxJs字符串转Object
+	Settings.NextHour.HTTPHeaders = Settings.NextHour?.HTTPHeaders ?
+		JSON.parse(Settings.NextHour.HTTPHeaders) : Settings.NextHour.HTTPHeaders // BoxJs字符串转Object
 	if (Settings.NextHour?.Debug?.Switch) Settings.NextHour.Debug.Switch = JSON.parse(Settings.NextHour?.Debug?.Switch ?? false) // BoxJs字符串转Boolean
 	Settings.AQI.Switch = JSON.parse(Settings.AQI.Switch) // BoxJs字符串转Boolean
 	Settings.Map.AQI = JSON.parse(Settings.Map.AQI) // BoxJs字符串转Boolean
