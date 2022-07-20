adc661d5c67c159e774de2a505c2d4c71236cf70
WordlessEcho <wordless@echo.moe>
2022-07-20 20:49:49 +0800

    Fix airQuality for Apple APIv1

--- js/Weather.response.beta.js
+++ js/Weather.response.beta.js
@@ -6,7 +6,7 @@
 // eslint-disable-next-line
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}isShadowrocket(){return"undefined"!=typeof $rocket}isStash(){return"undefined"!=typeof $environment&&$environment["stash-version"]}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),n={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(n,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){if(t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let s=require("iconv-lite");this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:i,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:i,statusCode:r,headers:o,rawBody:h},s.decode(h,this.encoding))},t=>{const{message:i,response:r}=t;e(i,r,r&&s.decode(r.rawBody,this.encoding))})}}post(t,e=(()=>{})){const s=t.method?t.method.toLocaleLowerCase():"post";if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient[s](t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method=s,this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let i=require("iconv-lite");this.initGotEnv(t);const{url:r,...o}=t;this.got[s](r,o).then(t=>{const{statusCode:s,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:s,statusCode:r,headers:o,rawBody:h},i.decode(h,this.encoding))},t=>{const{message:s,response:r}=t;e(s,r,r&&i.decode(r.rawBody,this.encoding))})}}time(t,e=null){const s=e?new Date(e):new Date;let i={"M+":s.getMonth()+1,"d+":s.getDate(),"H+":s.getHours(),"m+":s.getMinutes(),"s+":s.getSeconds(),"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(s.getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in i)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?i[e]:("00"+i[e]).substr((""+i[e]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl,i=t["update-pasteboard"]||t.updatePasteboard;return{"open-url":e,"media-url":s,"update-pasteboard":i}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
 
-const $ = new Env('Apple Weather v4.0.0-beta');
+const $ = new Env('Apple Weather v4.0.0-beta2');
 
 // https://github.com/VirgilClyne/VirgilClyne/blob/main/function/URL/URLs.embedded.min.js
 // noinspection
@@ -2433,15 +2433,29 @@ const fixQweatherCo = (unit, amount) => {
   return amount;
 };
 
+const convertV1Pollutants = (pollutants) => {
+  const units = ['ppb', 'µg/m3'];
+  const validPollutants = Array.isArray(pollutants) ? pollutants.filter(
+    (pollutant) => (
+      units.includes(pollutant?.unit) && typeof pollutant?.name === 'string' && pollutant.name.length > 0
+      && isNonNanNumber(pollutant?.amount) && pollutant.amount >= 0
+    ),
+  ) : [];
+
+  return validPollutants.map((pollutant) => ({
+    ...pollutant, unit: pollutant.unit === 'µg/m3' ? 'microgramsPerM3' : pollutant.unit,
+  }));
+};
+
 /**
  * Convert pollutants from Apple to specific EPA standard
  * @param {aqiStandard} standard
- * @param {Object.<iosPollutantNames, pollutantV2>} pollutants
+ * @param {pollutantV2[]} pollutants
  * @return {Object} - Object for {@link toAirQuality}
  */
 const appleToEpaAirQuality = (standard, pollutants) => {
   if (
-    typeof standard !== 'object' || typeof pollutants !== 'object'
+    typeof standard !== 'object' || !Array.isArray(pollutants)
     || typeof standard?.CONCENTRATIONS !== 'object' || typeof standard?.AQI_LEVELS !== 'object'
   ) {
     return {};
@@ -2460,12 +2474,13 @@ const appleToEpaAirQuality = (standard, pollutants) => {
     return {};
   }
 
-  const pollutantsValue = Object.values(pollutants).filter((pollutant) => (
-    Object.keys(validConcentrations).includes(pollutant?.name) && typeof pollutant?.unit === 'string'
-    && pollutant.unit.length > 0 && isNonNanNumber(pollutant?.amount) && pollutant.amount >= 0
+  const units = ['ppb', 'microgramsPerM3'];
+  const validPollutants = pollutants.filter((pollutant) => (
+    Object.keys(validConcentrations).includes(pollutant?.name) && units.includes(pollutant?.unit)
+    && isNonNanNumber(pollutant?.amount) && pollutant.amount >= 0
   ));
 
-  const aqis = toEpaAqis(validConcentrations, pollutantsValue);
+  const aqis = toEpaAqis(validConcentrations, validPollutants);
   if (!isNonNanNumber(aqis.index) || aqis.index < 0) {
     return {};
   }
@@ -2710,13 +2725,13 @@ const colorfulCloudsToAqi = (
    * Convert data from {@link getCcAirQuality} to {@link pollutantV2} object for Apple Weather
    * @author WordlessEcho <wordless@echo.moe>
    * @param {Object} airQuality - Pollutants data from {@link getCcAirQuality}
-   * @return {Object.<iosPollutantNames, pollutantV2>|{}} -
+   * @return {pollutantV2[]|[]} -
    * Object for `airQuality.pollutants` of Apple Weather
    */
   const toPollutants = (airQuality) => (typeof airQuality === 'object'
-    ? Object.fromEntries(Object.entries(airQuality)
+    ? Object.entries(airQuality)
       .filter(([key]) => key !== 'aqi')
-      .map(([pollutantName, amount]) => ([pollutantName, {
+      .map(([pollutantName, amount]) => ({
         name: pollutantName,
         amount: pollutantName === 'CO' ? pollutantUnitConverterUs(
           'milligramsPerM3',
@@ -2725,8 +2740,8 @@ const colorfulCloudsToAqi = (
           null,
         ) : amount,
         unit: 'microgramsPerM3',
-      }])))
-    : {});
+      }))
+    : []);
 
   const airQuality = getCcAirQuality(realtimeAndHistoryData);
   const standard = getCcStandard(airQuality.aqi.usa >= 0, aqiForceChn);
@@ -2739,7 +2754,7 @@ const colorfulCloudsToAqi = (
 
   const categoryIndex = toAqiLevel(Object.values(standard.AQI_LEVELS), aqi);
   const pollutants = toPollutants(airQuality);
-  const primaryPollutant = toEpaAqis(standard.CONCENTRATIONS, Object.values(pollutants))?.primary;
+  const primaryPollutant = toEpaAqis(standard.CONCENTRATIONS, pollutants)?.primary;
 
   return {
     isSignificant: categoryIndex >= (isNonNanNumber(standard.SIGNIFICANT_LEVEL)
@@ -2822,7 +2837,7 @@ const waqiToAqi = (feedData) => {
     url: typeof feedData?.data?.city?.url === 'string' && feedData.data.city.url.length > 0
       ? feedData.data.city.url : 'https://aqicn.org/',
     // Pollutant data from WAQI is AQI not amount
-    pollutants: {},
+    pollutants: [],
     ...(Object.keys(toApplePollutantName).includes(feedData?.data?.dominentpol)
       && { primary: toApplePollutantName[feedData.data.dominentpol] }),
     sourceName: typeof feedData?.data?.city?.name === 'string' && feedData.data.city.name.length > 0
@@ -3582,34 +3597,24 @@ const toMetadata = (appleApiVersion, metadataObject) => {
  * @return {Object}
  */
 const toAirQuality = (appleApiVersion, aqiObject) => {
-  const getUnits = (apiVersion) => {
-    const sharedUnit = ['ppb'];
-    switch (apiVersion) {
-      case 1:
-        return sharedUnit.concat(['µg/m3']);
-      case 2:
-      case 3:
-        return sharedUnit.concat(['microgramsPerM3']);
-      default:
-        return [];
-    }
-  };
-
   const iosPollutantNames = [
     'NO2', 'NO', 'NOX', 'PM2.5', 'SO2', 'OZONE', 'PM10', 'CO',
   ];
+  const units = ['ppb', 'microgramsPerM3'];
   const comparisonValues = ['better', 'same', 'worse', 'unknown'];
   const sourceTypes = ['station', 'modeled'];
+
+  const validPollutants = Array.isArray(aqiObject.pollutants) ? aqiObject.pollutants.filter(
+    (pollutant) => (
+      units.includes(pollutant?.unit) && typeof pollutant?.name === 'string' && pollutant.name.length > 0
+      && isNonNanNumber(pollutant?.amount) && pollutant.amount >= 0
+    ),
+  ) : [];
 
   const sharedAirQuality = {
     ...(typeof aqiObject?.isSignificant === 'boolean' && { isSignificant: aqiObject.isSignificant }),
     // TODO
     ...(typeof aqiObject?.url === 'string' && aqiObject.url.length > 0 && { learnMoreURL: aqiObject.url }),
-    ...(Object.keys(aqiObject?.pollutants ?? {}).length > 0 && {
-      pollutants: Object.fromEntries(Object.entries(aqiObject.pollutants).filter(
-        ([, info]) => getUnits(appleApiVersion).includes(info?.unit),
-      )),
-    }),
     ...(iosPollutantNames.includes(aqiObject?.primary) && { primaryPollutant: aqiObject.primary }),
     // TODO: source was removed after APIv3
     ...(typeof aqiObject?.sourceName === 'string' && aqiObject.sourceName.length > 0
@@ -3628,6 +3633,12 @@ const toAirQuality = (appleApiVersion, aqiObject) => {
           // TODO: find out all scale
           ...(typeof aqiObj?.scale === 'string' && aqiObj.scale.length > 0
             && { airQualityScale: aqiObj.scale }),
+          ...({
+            pollutants: Object.fromEntries(validPollutants.map((pollutant) => ([
+              pollutant.name,
+              { ...pollutant, unit: pollutant.unit === 'microgramsPerM3' ? 'µg/m3' : pollutant.unit },
+            ]))),
+          }),
         };
       case 2:
       case 3:
@@ -3643,6 +3654,11 @@ const toAirQuality = (appleApiVersion, aqiObject) => {
           ...(typeof aqiObj?.scale === 'string' && aqiObj.scale.length > 0
             && { scale: aqiObj.scale }),
           ...(sourceTypes.includes(aqiObj?.sourceType) && { sourceType: aqiObj.sourceType }),
+          ...({
+            pollutants: Object.fromEntries(validPollutants.map((pollutant) => ([
+              pollutant.name, pollutant,
+            ]))),
+          }),
         };
       default:
         return {};
@@ -4574,6 +4590,8 @@ const toResponseBody = (envs, request, response) => {
       metadata: { ...airQuality?.[METADATA], ...modifiedAirQuality?.[METADATA] },
     };
     const mergedScale = mergedAirQuality?.[AQI_SCALE];
+    const pollutants = typeof mergedAirQuality?.[POLLUTANTS] === 'object'
+      ? Object.values(mergedAirQuality[POLLUTANTS]) : [];
     const localConvertedAirQuality = {
       ...mergedAirQuality,
       ...(settings.aqi.local.switch && typeof mergedAirQuality?.[POLLUTANTS] === 'object'
@@ -4582,7 +4600,7 @@ const toResponseBody = (envs, request, response) => {
         )
         && toAirQuality(appleApiVersion, appleToEpaAirQuality(
           settingsToAqiStandard[settings.aqi.local.standard],
-          mergedAirQuality[POLLUTANTS],
+          appleApiVersion === 1 ? convertV1Pollutants(pollutants) : pollutants,
         ))),
     };
     const aqiLevels = scaleToAqiStandard[localConvertedAirQuality?.[AQI_SCALE]]?.AQI_LEVELS;
