3dd3b7a3284c4b7a414455f0b51f465fe177f002
Virgil Clyne <Virgil@nanocat.me>
2023-02-09 16:10:06 +0800

    fix(Siri): warm(unknown) & render(unknown)
    Update Siri.request.js
    Update Siri.plugin
    Update Siri.qxrewrite
    Update Siri.sgmodule
    Update Siri.stoverride

--- js/Siri.request.js
+++ js/Siri.request.js
@@ -1,7 +1,7 @@
 /*
 README:https://github.com/VirgilClyne/iRingo
 */
-const $ = new Env("Apple Siri v2.1.8-request");
+const $ = new Env("Apple Siri v2.1.9-request");
 const URL = new URLs();
 const DataBase = {
 	"Location":{
@@ -43,6 +43,8 @@ const DataBase = {
 		$.log(url.path);
 		switch (url.path) {
 			case "bag":
+			case "warm":
+			case "render":
 			case "flight":
 				break;
 			case "search": //Search
