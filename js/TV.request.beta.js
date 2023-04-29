1c8d07b4f41764c072566316cdbd205e65a7763d
Virgil Clyne <Virgil@nanocat.me>
2023-04-29 13:26:52 +0800

    docs(database): Storefront
    Update TV.request.beta.js
    Update TV.response.beta.js

--- js/TV.request.beta.js
+++ js/TV.request.beta.js
@@ -1,7 +1,7 @@
 /*
 README: https://github.com/VirgilClyne/iRingo
 */
-const $ = new Env(" iRingo: 📺 TV v3.0.0(8) request.beta");
+const $ = new Env(" iRingo: 📺 TV v3.0.0(10) request.beta");
 const URL = new URLs();
 const DataBase = {
 	"Location":{
@@ -9,7 +9,10 @@ const DataBase = {
 	},
 	"Weather":{
 		"Settings":{"Switch":"true","NextHour":{"Switch":true},"AQI":{"Switch":true,"Mode":"WAQI Public","Location":"Station","Auth":null,"Scale":"EPA_NowCast.2204"},"Map":{"AQI":false}},
-		"Configs":{"Availability":["currentWeather","forecastDaily","forecastHourly","history","weatherChange","forecastNextHour","severeWeather","airQuality"],"Pollutants":{"co":"CO","no":"NO","no2":"NO2","so2":"SO2","o3":"OZONE","nox":"NOX","pm25":"PM2.5","pm10":"PM10","other":"OTHER"}}
+		"Configs":{
+			"Availability":["currentWeather","forecastDaily","forecastHourly","history","weatherChange","forecastNextHour","severeWeather","airQuality"],
+			"Pollutants":{"co":"CO","no":"NO","no2":"NO2","so2":"SO2","o3":"OZONE","nox":"NOX","pm25":"PM2.5","pm10":"PM10","other":"OTHER"}
+		}
 	},
 	"Siri":{
 		"Settings":{"Switch":"true","CountryCode":"SG","Domains":["web","itunes","app_store","movies","restaurants","maps"],"Functions":["flightutilities","lookup","mail","messages","news","safari","siri","spotlight","visualintelligence"],"Safari_Smart_History":true},
@@ -59,7 +62,7 @@ const DataBase = {
 	"Default": {
 		"Settings":{"Switch":"true"},
 		"Configs":{
-			"Storefront":{"AF":"143610","AL":"143575","AO":"143564","AI":"143538","AG":"143540","AR":"143505","AM":"143524","AU":"143460","AT":"143445","AZ":"143568","BA":"143612","BS":"143539","BH":"143559","BB":"143541","BD":"143490","BY":"143565","BE":"143446","BZ":"143555","BJ":"143576","BM":"143542","BT":"143577","BO":"143556","BW":"143525","BR":"143503","VG":"143543","BN":"143560","BG":"143526","BF":"143578","CA":"143455","CI":"143527","CM":"143574","CV":"143580","KY":"143544","TD":"143581","CL":"143483","CN":"143465","CO":"143501","CG":"143582","CR":"143495","HR":"143494","CY":"143557","CZ":"143489","DK":"143458","DM":"143545","DO":"143508","DZ":"143563","EC":"143509","EG":"143516","SV":"143506","EE":"143518","FJ":"143583","FI":"143447","FR":"143442","GM":"143584","DE":"143443","GH":"143573","GR":"143448","GD":"143546","GT":"143504","GW":"143585","GY":"143553","HN":"143510","HK":"143463","HU":"143482","IS":"143558","IN":"143467","ID":"143476","IE":"143449","IL":"143491","IT":"143450","JM":"143511","JP":"143462","JO":"143528","KH":"143579","KR":"143466","KZ":"143517","KE":"143529","KW":"143493","KG":"143586","LA":"143587","LV":"143519","LB":"143497","LR":"143588","LT":"143520","LI":"143522","LU":"143451","MO":"143515","MK":"143530","MG":"143531","MW":"143589","MY":"143473","MV":"143488","ML":"143532","MT":"143521","MR":"143590","MU":"143533","MX":"143468","FM":"143591","MD":"143523","MN":"143592","MS":"143547","MZ":"143593","NA":"143594","NP":"143484","NL":"143452","NZ":"143461","NI":"143512","NE":"143534","NG":"143561","NO":"143457","OM":"143562","PK":"143477","PW":"143595","PA":"143485","PG":"143597","PY":"143513","PE":"143507","PH":"143474","PL":"143478","PT":"143453","QA":"143498","RO":"143487","RU":"143469","ST":"143598","SA":"143479","SN":"143535","SC":"143599","SL":"143600","SG":"143464","SK":"143496","SI":"143499","SB":"143601","ZA":"143472","KP":"143466","ES":"143454","LK":"143486","KN":"143548","LC":"143549","VC":"143550","SR":"143554","SZ":"143602","SE":"143456","CH":"143459","TW":"143470","TJ":"143603","TZ":"143572","TH":"143475","TT":"143551","TN":"143536","TR":"143480","TM":"143604","TC":"143552","AE":"143481","UG":"143537","UA":"143492","GB":"143444","US":"143441","UY":"143514","UZ":"143566","VE":"143502","VN":"143471","YE":"143571","ZW":"143605","CD":"143613","GA":"143614","GF":"143615","IQ":"143617","XK":"143624","LY":"143567","ME":"143619","MA":"143620","MM":"143570","NR":"143606","RW":"143621","RS":"143500","TO":"143608","VU":"143609","ZM":"143622"}
+			"Storefront":[["AE","143481"],["AF","143610"],["AG","143540"],["AI","143538"],["AL","143575"],["AM","143524"],["AO","143564"],["AR","143505"],["AT","143445"],["AU","143460"],["AZ","143568"],["BA","143612"],["BB","143541"],["BD","143490"],["BE","143446"],["BF","143578"],["BG","143526"],["BH","143559"],["BJ","143576"],["BM","143542"],["BN","143560"],["BO","143556"],["BR","143503"],["BS","143539"],["BT","143577"],["BW","143525"],["BY","143565"],["BZ","143555"],["CA","143455"],["CD","143613"],["CG","143582"],["CH","143459"],["CI","143527"],["CL","143483"],["CM","143574"],["CN","143465"],["CO","143501"],["CR","143495"],["CV","143580"],["CY","143557"],["CZ","143489"],["DE","143443"],["DK","143458"],["DM","143545"],["DO","143508"],["DZ","143563"],["EC","143509"],["EE","143518"],["EG","143516"],["ES","143454"],["FI","143447"],["FJ","143583"],["FM","143591"],["FR","143442"],["GA","143614"],["GB","143444"],["GD","143546"],["GF","143615"],["GH","143573"],["GM","143584"],["GR","143448"],["GT","143504"],["GW","143585"],["GY","143553"],["HK","143463"],["HN","143510"],["HR","143494"],["HU","143482"],["ID","143476"],["IE","143449"],["IL","143491"],["IN","143467"],["IQ","143617"],["IS","143558"],["IT","143450"],["JM","143511"],["JO","143528"],["JP","143462"],["KE","143529"],["KG","143586"],["KH","143579"],["KN","143548"],["KP","143466"],["KR","143466"],["KW","143493"],["KY","143544"],["KZ","143517"],["TC","143552"],["TD","143581"],["TJ","143603"],["TH","143475"],["TM","143604"],["TN","143536"],["TO","143608"],["TR","143480"],["TT","143551"],["TW","143470"],["TZ","143572"],["LA","143587"],["LB","143497"],["LC","143549"],["LI","143522"],["LK","143486"],["LR","143588"],["LT","143520"],["LU","143451"],["LV","143519"],["LY","143567"],["MA","143620"],["MD","143523"],["ME","143619"],["MG","143531"],["MK","143530"],["ML","143532"],["MM","143570"],["MN","143592"],["MO","143515"],["MR","143590"],["MS","143547"],["MT","143521"],["MU","143533"],["MV","143488"],["MW","143589"],["MX","143468"],["MY","143473"],["MZ","143593"],["NA","143594"],["NE","143534"],["NG","143561"],["NI","143512"],["NL","143452"],["NO","143457"],["NP","143484"],["NR","143606"],["NZ","143461"],["OM","143562"],["PA","143485"],["PE","143507"],["PG","143597"],["PH","143474"],["PK","143477"],["PL","143478"],["PT","143453"],["PW","143595"],["PY","143513"],["QA","143498"],["RO","143487"],["RS","143500"],["RU","143469"],["RW","143621"],["SA","143479"],["SB","143601"],["SC","143599"],["SE","143456"],["SG","143464"],["SI","143499"],["SK","143496"],["SL","143600"],["SN","143535"],["SR","143554"],["ST","143598"],["SV","143506"],["SZ","143602"],["UA","143492"],["UG","143537"],["US","143441"],["UY","143514"],["UZ","143566"],["VC","143550"],["VE","143502"],["VG","143543"],["VN","143471"],["VU","143609"],["XK","143624"],["YE","143571"],["ZA","143472"],["ZM","143622"],["ZW","143605"]]
 		}
 	}
 };
@@ -107,7 +110,7 @@ let $response = undefined;
 										case "uts/v2/favorites/add":
 										case "uts/v2/favorites/remove":
 											Type = "Sports";
-											if ($request.body) $request.body = $request.body.replace(/sf=[\d]{6}/, `sf=${Configs.Storefront[Settings.CountryCode[Type]]}`);
+											if ($request.body) $request.body = $request.body.replace(/sf=[\d]{6}/, `sf=${Configs.Storefront.get(Settings.CountryCode[Type])}`);
 											break;
 									};
 									break;
@@ -129,10 +132,10 @@ let $response = undefined;
 										case "uts/v3/configurations":
 											Type = "Configs";
 											if (Settings.CountryCode[Type] !== "AUTO") {
-												if (url.params.region) url.params.region = Settings.CountryCode[Type] || url.params.region
-												if (url.params.country) url.params.country = Settings.CountryCode[Type] || url.params.country
-											}
-											if (url.params.sfh) url.params.sfh = (Configs.Storefront?.[Settings.CountryCode[Type]]) ? url.params.sfh.replace(/\d{6}/, Configs.Storefront[Settings.CountryCode[Type]]) : url.params.sfh
+												if (url.params.region) url.params.region = Settings.CountryCode[Type] ?? url.params.region;
+												if (url.params.country) url.params.country = Settings.CountryCode[Type] ?? url.params.country;
+												if (url.params.sfh) url.params.sfh = url.params.sfh.replace(/\d{6}/, Configs.Storefront.get(Settings.CountryCode[Type]) ?? url.params.sfh);
+											};
 											$.log(`🚧 ${$.name}, 调试信息`, `region = ${url.params.region}, country = ${url.params.country}, sfh = ${url.params.sfh}`, "")
 											break;
 										case "uts/v3/user/settings":
@@ -223,7 +226,7 @@ let $response = undefined;
 								case "uts/v2/favorites/add":
 								case "uts/v2/favorites/remove":
 									Type = "Sports";
-									if ($request.body) $request.body = $request.body.replace(/sf=[\d]{6}/, `sf=${Configs.Storefront[Settings.CountryCode[Type]]}`);
+									if ($request.body) $request.body = $request.body.replace(/sf=[\d]{6}/, `sf=${Configs.Storefront.get(Settings.CountryCode[Type])}`);
 									break;
 								case "uts/v3/search":
 								case "uts/v3/search/landing":
@@ -262,11 +265,11 @@ let $response = undefined;
 			$.log(`⚠ ${$.name}, Type = ${Type}, CC = ${Settings.CountryCode[Type]}`);
 			if ($request?.headers?.Host) $request.headers.Host = url.host;
 			if ($request?.headers?.["x-apple-store-front"]) {
-				$request.headers["x-apple-store-front"] = (Configs.Storefront?.[Settings.CountryCode[Type]])
-					? $request.headers["x-apple-store-front"].replace(/\d{6}/, Configs.Storefront[Settings.CountryCode[Type]])
+				$request.headers["x-apple-store-front"] = (Configs.Storefront.get(Settings.CountryCode[Type]))
+					? $request.headers["x-apple-store-front"].replace(/\d{6}/, Configs.Storefront.get(Settings.CountryCode[Type]))
 					: $request.headers["x-apple-store-front"];
 			};
-			if (url?.params?.sf) url.params.sf = Configs.Storefront[Settings.CountryCode[Type]] ?? url.params.sf
+			if (url?.params?.sf) url.params.sf = Configs.Storefront.get(Settings.CountryCode[Type]) ?? url.params.sf
 			if (url.params.locale) url.params.locale = Configs.Locale.get(Settings.CountryCode[Type]) ?? url.params.locale
 			$.log(`🚧 ${$.name}, 调试信息`, `sf = ${url.params.sf}, locale = ${url.params.locale}`, "")
 			$request.url = URL.stringify(url);
@@ -375,6 +378,7 @@ function setENV(name, platform, database) {
 	/***************** Configs *****************/
 	Configs.Locale = new Map(Configs.Locale);
 	for (let type in Configs.i18n) Configs.i18n[type] = new Map(Configs.i18n[type]);
+	Configs.Storefront = new Map(Configs.Storefront);
 	return { Settings, Caches, Configs };
 };
 
