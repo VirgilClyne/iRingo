74e042560167cf5c3b517e4c7e3ac075f46bd64d
Virgil Clyne <Virgil@nanocat.me>
2024-09-09 15:49:14 +0800

    test(Siri): SiriPegasusRequest
    Create Apple.Parsec.Siri.V2alpha.SiriPegasusRequest.d.ts
    Create Apple.Parsec.Siri.V2alpha.SiriPegasusRequest.js
    Create Apple.Parsec.Siri.V2alpha.SiriPegasusContext.d.ts
    Create Apple.Parsec.Siri.V2alpha.SiriPegasusContext.js
    Create Apple.Parsec.Search.PegasusQueryContext.d.ts
    Create Apple.Parsec.Search.PegasusQueryContext.js
    Update Siri.request.beta.js
    Create PegasusAPI.mjs
    Create any.d.ts
    Create any.js

---
+++ src/class/PegasusAPI.mjs
@@ -0,0 +1,48 @@
+import { log } from "../utils/utils.mjs";
+//import { MESSAGE_TYPE, reflectionMergePartial, BinaryReader, WireType, UnknownFieldHandler, isJsonObject, typeofJsonValue, jsonWriteOptions, MessageType } from "@protobuf-ts/runtime";
+import { SiriPegasusRequest } from "../protobuf/Apple.Parsec.Siri.V2alpha.SiriPegasusRequest.js";
+import { SiriPegasusContext } from "../protobuf/Apple.Parsec.Siri.V2alpha.SiriPegasusContext";
+import { PegasusQueryContext } from "../protobuf/Apple.Parsec.Search.PegasusQueryContext";
+export default class PegasusAPI {
+    static Name = "PegasusAPI";
+    static Version = "1.0.0";
+	static Author = "VirgilClyne";
+    static decode(rawBody = new Uint8Array([]), message = "SiriPegasusRequest") {
+        log("☑️ PegasusAPI.decode", "");
+        let body = {};
+        switch (message) {
+            case "SiriPegasusRequest":
+                body = SiriPegasusRequest.fromBinary(rawBody);
+                break;
+            case "SiriPegasusContext":
+                body = SiriPegasusContext.fromBinary(rawBody);
+                break;
+            case "PegasusQueryContext":
+                body = PegasusQueryContext.fromBinary(rawBody);
+                break;
+            default:
+                throw new Error(`Unknown message type: ${message}`);
+        };
+        log("✅ PegasusAPI.decode", "");
+        return body;
+    };
+    static encode(body = {}, message = "SiriPegasusRequest") {
+        log("☑️ PegasusAPI.encode", "");
+        let rawBody = new Uint8Array([]);
+        switch (message) {
+            case "SiriPegasusRequest":
+                rawBody = SiriPegasusRequest.toBinary(body);
+                break;
+            case "SiriPegasusContext":
+                rawBody = SiriPegasusContext.toBinary(body);
+                break
+            case "PegasusQueryContext":
+                rawBody = PegasusQueryContext.toBinary(body);
+                break;
+            default:
+                throw new Error(`Unknown message type: ${message}`);
+        };
+        log("✅ PegasusAPI.encode", "");
+        return rawBody;
+    };
+}
