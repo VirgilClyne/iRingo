08ec38df23646fe8ebebe5cd9077e274194fcf16
Virgil Clyne <Virgil@nanocat.me>
2024-09-11 14:16:42 +0800

    fix(fucntion): parseWeatherKitURL
    Update WeatherKit.response.beta.js
    Update parseWeatherKitURL.mjs
    Update WeatherKit.response.js

--- js/WeatherKit.response.beta.js
+++ js/WeatherKit.response.beta.js
@@ -1994,11 +1994,11 @@ function setENV(name, platforms, database) {
 	return { Settings, Caches, Configs };
 }
 
-function parseWeatherKitURL(url = $request.url) {
+function parseWeatherKitURL(url = new URL($request.url)) {
     console.log(`☑️ parseWeatherKitURL`, "");
-    const RegExp = /^\/api\/(?<version>v1|v2|v3)\/(availability|weather)\/(?<language>[\w-_]+)\/(?<latitude>-?\d+\.?\d*)\/(?<longitude>-?\d+\.?\d*)(\?.*(?<country>country=[A-Z]{2})?.*)?/i;
-    const LanguageRegExp = /^(?<language>\w+(-\w+)?)-(?<country>[A-Z]{2})$/i;
-    const Parameters = (url?.pathname || url).match(RegExp)?.groups;
+    const RegExp = /^\/api\/(?<version>v1|v2|v3)\/(availability|weather)\/(?<language>\w+)(?:-\w+)?(-(?<country>[A-Z]{2}))?\/(?<latitude>-?\d+\.?\d*)\/(?<longitude>-?\d+\.?\d*)$/i;
+    //const LanguageRegExp = /^(?<language>\w+(-\w+)?)-(?<country>[A-Z]{2})$/i;
+    const Parameters = url?.pathname.match(RegExp)?.groups;
     let result = {
         "version": Parameters?.version,
         "language": Parameters?.language,
@@ -2007,9 +2007,9 @@ function parseWeatherKitURL(url = $request.url) {
         "country": Parameters?.country || url?.searchParams?.get("country")
     };
     //console.log(JSON.stringify(result, null, 2), "");
-    const LanguageParameters = result.language.match(LanguageRegExp)?.groups;
-    result.language = LanguageParameters.language;
-    result.country = result.country || LanguageParameters.country;
+    //const LanguageParameters = result.language.match(LanguageRegExp)?.groups;
+    //result.language = LanguageParameters.language;
+    //result.country = result.country || LanguageParameters.country
     console.log(`✅ parseWeatherKitURL\n🟧version: ${result.version} 🟧language: ${result.language} 🟧country: ${result.country}\n🟧latitude: ${result.latitude} 🟧longitude: ${result.longitude}\n`, "");
     return result;
 }
