a7f2486a1e7687e844b28faf4f7c5ac062a2d755
Virgil Clyne <Virgil@nanocat.me>
2024-10-11 16:18:45 +0800

    build: rm others
    Delete package-lock.json

--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-import defaultConfig from './rollup.default.config.js';
-import debugConfig from './rollup.debug.config.js';
-
-export default commandLineArgs => {
-	if (commandLineArgs.configDebug === true) return debugConfig;
-	else return defaultConfig;
-};
--- rollup.default.config.js
+++
@@ -1,7 +0,0 @@
-import json from '@rollup/plugin-json';
-import commonjs from "@rollup/plugin-commonjs";
-import terser from '@rollup/plugin-terser';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-
-export default [
-];
